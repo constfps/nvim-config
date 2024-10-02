74358c4096c58a3ae4803b7b58e046f418fdee0c
const <constfps@gmail.com>
2024-10-01 20:37:35 -0700

    changed deprecated ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
             require("mason-lspconfig").setup({
                 ensure_installed = {
                     "lua_ls",
-                    "tsserver",
+                    "ts_ls",
                     "clangd",
                     "cssls"
                 }
